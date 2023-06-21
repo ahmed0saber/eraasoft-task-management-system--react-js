cb429438b8ca9ae08e454f1deacd7e10a35f82fa
Ahmed Saber <ahmed0saber33@gmail.com>
2023-06-22 02:52:06 +0300

    Style ErrorPage

--- eraasoft-auth/src/pages/ErrorPage.jsx
+++ eraasoft-auth/src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
     return (
-        <div>
+        <div className='error-page'>
             <h1>404, page not found</h1>
             <Link to="/">Back To Home</Link>
         </div>
