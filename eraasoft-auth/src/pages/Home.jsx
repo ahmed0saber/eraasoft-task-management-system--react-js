0a7fe254644e3701cbe3910e6db70a9918ef927a
Ahmed Saber <ahmed0saber33@gmail.com>
2023-06-22 03:00:19 +0300

    Style HomePage

--- eraasoft-auth/src/pages/Home.jsx
+++ eraasoft-auth/src/pages/Home.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+        <div className='home-page'>
+            <h1>Task Management System</h1>
+            <p>Here, you can easily manage your tasks, you can add new task, edit an existing one, or delete it. Log in now to enjoy all our features.</p>
+            <div className='btns'>
+                <Link to="/login">Log in</Link>
+                <Link to="/register">Register</Link>
+            </div>
         </div>
     )
 }
