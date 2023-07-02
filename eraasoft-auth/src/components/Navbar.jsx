dedd237948f71675e61a03d704969ed935e43aff
Ahmed Saber <ahmed0saber33@gmail.com>
2023-07-02 19:50:44 +0300

    Toggle navbar on auth

--- eraasoft-auth/src/components/Navbar.jsx
+++ eraasoft-auth/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
+    const userContext = useContext(UserContext);
     return (
         <header className='navbar'>
             <h2><Link to="/">Task Management System</Link></h2>
             <nav>
-                <NavLink to="/login">Log in</NavLink>
+                {userContext.user.name ? <span>Hello, {userContext.user.name}!</span> : <NavLink to="/login">Log in</NavLink>}
             </nav>
         </header>
     )
