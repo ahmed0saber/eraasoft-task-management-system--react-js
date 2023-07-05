7d21450f9e0c4c8e65937c50c8bf54605d7278fd
Ahmed Saber <ahmed0saber33@gmail.com>
2023-07-05 03:51:14 +0300

    Del - display user details in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,11 @@
-import React, { useContext } from "react";
-import UserContext from "../context/UserContext";
+import React from "react";
 import AllTasks from "../components/AllTasks";
 
 function Profile() {
-    const userContext = useContext(UserContext);
-
     return (
         <div className="profile-container">
-            <h1>Welcome, {userContext.user.name}!</h1>
-            <p>Your email is: {userContext.user.email}</p>
+            <h1>Welcome, user!</h1>
+            <p>Your email is: email</p>
             <button className="logout-btn">Logout</button>
             <AllTasks />
         </div>
