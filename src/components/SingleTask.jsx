5464b43cc31e4d24d87349ea060040f89f8dc750
Ahmed Saber <ahmed0saber33@gmail.com>
2023-07-05 03:16:47 +0300

    Del - confirm editing a task

--- src/components/SingleTask.jsx
+++ src/components/SingleTask.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import Cookies from "js-cookie";
 import axios from 'axios';
 
 export default function SingleTask({ task, getTasksFromAPI }) {
     const [isBeingEdited, setIsBeingEdited] = useState(false)
-    const titleRef = useRef()
-    const descriptionRef = useRef()
 
     const deleteTask = (id) => {
         const accessToken = Cookies.get('access_token');
@@ -30,36 +28,12 @@ export default function SingleTask({ task, getTasksFromAPI }) {
         setIsBeingEdited(true)
     }
 
-    const confirmEdit = (id) => {
-        const accessToken = Cookies.get('access_token');
-
-        axios
-            .put(`https://fmb.eraasoft.com/api/tasks/${id}?token=${accessToken}`, {
-                title: titleRef.current.textContent,
-                description: descriptionRef.current.textContent,
-                priority: "high"
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                }
-            })
-            .then(() => {
-                setIsBeingEdited(false)
-                getTasksFromAPI()
-            })
-            .catch(() => {
-                alert("Something went wrong, please try again.");
-            });
-    }
-
     return (
         <div className='single-task'>
-            <p className='task-title' ref={titleRef} contentEditable={isBeingEdited}>{task.title}</p>
-            <p className='task-description' ref={descriptionRef} contentEditable={isBeingEdited}>{task.description}</p>
+            <p className='task-title' contentEditable={isBeingEdited}>{task.title}</p>
+            <p className='task-description' contentEditable={isBeingEdited}>{task.description}</p>
             <div className='action-btns-container'>
-                {isBeingEdited ? <button className='confirm-btn' onClick={() => confirmEdit(task.id)}>Confirm</button> 
+                {isBeingEdited ? <button className='confirm-btn'>Confirm</button> 
                 : <button className='edit-btn' onClick={() => editTask()}>Edit</button>}
                 <button className='delete-btn' onClick={() => deleteTask(task.id)}>Delete</button>
             </div>
