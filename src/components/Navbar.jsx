265ac3c413d8a745b0037d0a7c4df4fcae3c39b3
Ahmed Saber <ahmed0saber33@gmail.com>
2023-07-05 03:55:21 +0300

    Del - toggle navbar login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,12 @@
-import React, { useContext } from "react";
-import UserContext from "../context/UserContext";
+import React from "react";
 import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
-    const userContext = useContext(UserContext);
     return (
         <header className='navbar'>
             <h2><Link to="/">Task Management System</Link></h2>
             <nav>
-                {userContext.user.name ? <span>Hi, {userContext.user.name}!</span> : <NavLink to="/login">Log in</NavLink>}
+                <NavLink to="/login">Log in</NavLink>
             </nav>
         </header>
     )
