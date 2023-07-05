af638b96aaa879575588e4e99e04cb4c0dde4a29
Ahmed Saber <ahmed0saber33@gmail.com>
2023-07-05 03:58:04 +0300

    Del - implement user registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,61 +1,14 @@
-import React, { useState, useContext } from "react";
-import Cookies from "js-cookie";
-import UserContext from "../context/UserContext";
-import { useNavigate, Link, Navigate } from "react-router-dom";
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const userContext = useContext(UserContext);
-  const navigate = useNavigate();
-
-  const handleRegister = () => {
-    axios
-        .post("https://fmb.eraasoft.com/api/register", {
-          name: name,
-          email: email,
-          password: password,
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-        })
-        .then((response) => {
-          const currentUser = {
-            name: response.data.user.name,
-            email: response.data.user.email
-          }
-          const expiresAt = Date.now() + response.data.token.expires_in * 60 * 1000;
-          Cookies.set("access_token", response.data.token.access_token, {
-            expires: expiresAt,
-            secure: true,
-            sameSite: "strict",
-          });
-          Cookies.set("user", JSON.stringify(currentUser), {
-            expires: expiresAt,
-            secure: true,
-            sameSite: "strict",
-          });
-          userContext.setUser(currentUser);
-
-          navigate("/profile");
-        })
-        .catch(() => {
-          alert("Something went wrong, please try again.");
-        });
-  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleRegister();
   };
-
-  if (Cookies.get('access_token') && Cookies.get('user')) {
-    return <Navigate to="/profile" replace />;
-  }
 
   return (
     <div className="auth-page">
